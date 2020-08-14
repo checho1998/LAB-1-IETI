5e5692eb086152e141f4828799f69fd55db0f894
checho1998 <c.hecho.28@hotmail.com>
2020-08-13 19:08:54 -0500

    empezar

---
+++ todo-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Todo} from './TodoList'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        	&lt;h1&gt;TODO React App&lt;/h1&gt;
+	</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,25 @@
+importar  Reaccionar  desde  'reaccionar' ; 
+import {Todo} from './Todo'
+
+export class TodoList extends React.Component{
+
+	constructor(props) {
+        super(props);
+    	}
+
+	render() {
+		const listTodo = this.props.todoList.map((tod) =>
+    			<li>
+
+			  <Todo text={tod.text}> priority = {tod.priority} dueDate = {tod.dueDate}/>
+      			  
+    			</li>
+  		);
+		return(
+		  <ul> {listTodo} </ul>
+		);
+
+	}
+ 
+
+}
